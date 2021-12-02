2e2e975262047078fb85caa7a9ec62160da00a9f
Enrique-RAMOS <84140676+Enrique-RAMOS@users.noreply.github.com>
2021-12-02 11:32:45 -0600

    New

---
+++ PryectoMovieTheater/Controllers/AdministradorwebController.cs
@@ -0,0 +1,41 @@
+﻿using PryectoMovieTheater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PryectoMovieTheater.Controllers
+{
+    public class AdministradorwebController : Controller
+    {
+        // GET: Administradorweb
+        public ActionResult UpData()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Recursos imagen)
+        {
+
+
+
+            String ruta = Server.MapPath("~/img/");
+            ruta += imagen.file.FileName;
+
+            imagen.file.SaveAs(ruta);
+
+            return View();
+        }
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
